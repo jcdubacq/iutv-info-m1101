b798cb84fdbd5986bc306eae43a7979fde7e86ee
Jean-Christophe Dubacq <jean-christophe.dubacq@ens-lyon.org>
2020-09-07 12:13:18 +0200

    No --user in install ; everybody is root nowadays (or uses venv)

--- install.sh
+++ install.sh
@@ -28,7 +28,7 @@ checkinstall() {
 
 pipinstall() {
     echo -n "[python:$@]"
-    $PIPNAME -q install $@ --user
+    $PIPNAME -q install $@
     x=$?
     if [ "$x" = 0 ]; then
         echo "...ok"
@@ -73,12 +73,12 @@ pipinstall git+git://github.com/mkrphys/ipython-tikzmagic.git
 pipinstall jupyter_contrib_nbextensions
 
 echo "[configuration]"
-jupyterconfig enable:nbextensions_configurator nbextensions_configurator enable --user
-jupyterconfig enable:widgetsnbextension nbextension enable --py widgetsnbextension --user
-jupyterconfig install:hide_code nbextension install --py hide_code --user
-jupyterconfig enable:hide_code nbextension enable --py hide_code --user
-jupyterconfig contrib:install contrib nbextension install --user
-jupyterconfig serverextension:hide_code serverextension enable --py hide_code --user
+jupyterconfig enable:nbextensions_configurator nbextensions_configurator enable
+jupyterconfig enable:widgetsnbextension nbextension enable --py widgetsnbextension
+jupyterconfig install:hide_code nbextension install --py hide_code
+jupyterconfig enable:hide_code nbextension enable --py hide_code
+jupyterconfig contrib:install contrib nbextension install
+jupyterconfig serverextension:hide_code serverextension enable --py hide_code
 jupyterconfig enable:nbextensions_configurator nbextensions_configurator enable
 jupyterconfig enable:toc2 nbextension enable toc2/main
 
